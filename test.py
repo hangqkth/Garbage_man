99df20a33b1dc90448abb8f5c3d0ebac10310086
Hang Qin <112768477+hangqkth@users.noreply.github.com>
2023-09-25 01:24:01 +0200

    add train and test, with a trained model

---
+++ test.py
@@ -0,0 +1,51 @@
+import torchvision.models as models
+from load_data import GarbageData
+import torch.nn as nn
+import torch
+import torch.utils.data as data
+from tqdm import tqdm
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    model = models.resnet34(pretrained=False)
+    num_feature = model.fc.in_features
+    model.fc = nn.Sequential(
+        nn.Linear(num_feature, 64),
+        nn.BatchNorm1d(64),
+        nn.ReLU(),
+        nn.Linear(64, 12))
+
+    test_data = np.load('processed_data/test_data.npy')
+    test_label = np.load('processed_data/test_label.npy')
+
+    test_loader = data.DataLoader(dataset=GarbageData(test_data, test_label), batch_size=32, shuffle=True)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    model_param = torch.load('./saved_model/garbage_net.pth')
+    model.load_state_dict(model_param)
+    model.to(device)
+    pred_list, true_list = [], []
+    for data, label in tqdm(test_loader):
+        model.eval()  # evaluating mode
+        with torch.no_grad():  # no gradient
+            data = data.to(device=device, dtype=torch.float32).permute(0, 3, 1, 2)
+            pred = model(data)
+            pred = np.argmax(pred.cpu().detach().numpy(), axis=1)
+            pred_list += pred.tolist()
+            true_list += label.numpy().tolist()
+
+    acc = accuracy_score(y_true=true_list, y_pred=pred_list)
+    print("\rTest finished, Test Accuracy: {:.4%} ".format(acc))
+
+    str_labels = ['battery', 'biological', 'brown-glass', 'cardboard', 'clothes', 'green-glass',
+                  'metal', 'paper', 'plastic', 'shoes', 'trash', 'white-glass']
+
+    disp = ConfusionMatrixDisplay.from_predictions(y_true=true_list, y_pred=pred_list, cmap=plt.cm.Blues,
+                                                   xticks_rotation="vertical", display_labels=str_labels)
+    plt.title("Test Accuracy: {:.4%} ".format(acc))
+    plt.show()
+
+
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import torch
 import torch.utils.data as data
 import time
 import numpy as np
+from sklearn.metrics import accuracy_score
 
 resnet18 = models.resnet18(pretrained=True)
 
@@ -26,10 +27,10 @@ def train_and_test(train_loader, val_loader, lr, model, epochs, criterion, devic
         for data, label in train_loader:
             batch += 1
             model.train()  # train mode
-            data = data.to(device=device, dtype=torch.float32)  # numpy to tensor
-            a_pred = model(data)  # forward propagation
+            data = data.to(device=device, dtype=torch.float32).permute(0, 3, 1, 2)  # numpy to tensor
+            pred = model(data)  # forward propagation
             optimizer.zero_grad()  # initialize optimizer
-            loss = criterion(a_pred, label.to(device=device))
+            loss = criterion(pred, label.to(device=device, dtype=torch.long))
             loss.backward()  # backward propagation
             optimizer.step()  # update model parameters
             # if loss < best_loss_train:
@@ -42,43 +43,51 @@ def train_and_test(train_loader, val_loader, lr, model, epochs, criterion, devic
             "start testing"
             if batch % 30 == 0:
                 loss_sum = 0
+                pred_list, true_list = [], []
                 for data, label in val_loader:
                     model.eval()  # evaluating mode
                     with torch.no_grad():  # no gradient
-                        data = data.to(device=device, dtype=torch.float32)
-                        a_pred = model(data)
-                        loss_test = criterion(a_pred, label.to(device=device))
+                        data = data.to(device=device, dtype=torch.float32).permute(0, 3, 1, 2)
+                        pred = model(data)
+                        loss_test = criterion(pred, label.to(device=device, dtype=torch.long))
                         loss_sum += loss_test
+                        pred = pred.cpu().detach().numpy()
+                        pred = np.argmax(pred, axis=1)
+                        pred_list += pred.tolist()
+                        true_list += label.numpy().tolist()
                 loss_avg = loss_sum / len(val_loader)
+                acc = accuracy_score(true_list, pred_list)
                 if loss_avg <= best_loss_test:
                     best_loss_test = loss_avg
-                    # torch.save(model.state_dict(), './model/model_saved/'+model_name+'.pth')
-                print(
-                    "\rTest Epoch: {:d} | Test loss: {:.4f} | Best evaluation loss: {:.6f}".format(epoch + 1, loss_avg,
-                                                                                                   best_loss_test))
+                    torch.save(model.state_dict(), './saved_model/garbage_net.pth')
+                print("\rTest Epoch: {:d} | Test loss: {:.4f} | Test Accuracy: {:.4%} | Best evaluation loss: {:.6f}".format(epoch + 1, loss_avg, acc, best_loss_test))
                 time.sleep(0.1)
 
 
 if __name__ == "__main__":
-    # 冻结参数的梯度
-    feature_extract = True
-    model = models.resnet18(pretrained=True)
-    # set_parameter_requires_grad(model, feature_extract)
-    train_data = np.load('processed_data/train_data.npy')
-    train_label = np.load('processed_data/train_label.npy')
-    val_data = np.load('processed_data/val_data.npy')
-    val_label = np.load('processed_data/val_label.npy')
-    train_loader = data.DataLoader(dataset=GarbageData(train_data, train_label), batch_size=32, shuffle=True)
-    val_loader = data.DataLoader(dataset=GarbageData(val_data, val_label), batch_size=32, shuffle=True)
+    # freeze resnet parameter or not
+    # feature_extract = True
+    # model = models.resnet18(pretrained=True)
+    model = models.resnet34(pretrained=True)
     num_feature = model.fc.in_features
     model.fc = nn.Sequential(
         nn.Linear(num_feature, 64),
         nn.BatchNorm1d(64),
         nn.ReLU(),
         nn.Linear(64, 12))
+
+    # set_parameter_requires_grad(model, feature_extract)
+    train_data = np.load('processed_data/train_data.npy')
+    train_label = np.load('processed_data/train_label.npy')
+    val_data = np.load('processed_data/val_data.npy')
+    val_label = np.load('processed_data/val_label.npy')
+    train_loader = data.DataLoader(dataset=GarbageData(train_data, train_label), batch_size=64, shuffle=True)
+    val_loader = data.DataLoader(dataset=GarbageData(val_data, val_label), batch_size=64, shuffle=True)
+
     criterion = nn.CrossEntropyLoss()
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    train_and_test(train_loader, val_loader, 0.001, model, 10, criterion, device)
+    model.to(device)
+    train_and_test(train_loader, val_loader, 1e-4, model, 10, criterion, device)
 
 
 
